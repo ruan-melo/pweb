34a8094c3a0db00ccbed30e0e091da990212026f
Ruan Tenório de Melo <ruantmelo@gmail.com>
2022-01-24 14:14:17 -0300

    porta dinamica do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
 import {app} from './app';
 
-app.listen(4000, () => {console.log('Server is running on port 4000')});
+const port = process.env.PORT || 4000
+
+app.listen(port, () => {console.log('Server is running on port ' + port)});
